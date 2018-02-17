7ba9fb4d56d2813b3914fb675ace6be2c50a33b8
databank <adrian@databank.ro>
2018-02-17 12:22:06 +0200

    custom handler: +request npm & debug

--- custom-handler/index.js
+++ custom-handler/index.js
@@ -1,20 +1,28 @@
-console.log('Loading function');
+
 var AWS = require('aws-sdk');
 const response = require('cfn-response');
 exports.handler = function(event, context) {
-    // create, update, and delete
-    console.log(event)
-    switch (event.RequestType) {
-        case 'Delete':
-            return response.send(event, context, response.SUCCESS);
-            break;
-        case 'Update':
-            return response.send(event, context, response.SUCCESS);
-            break;
-        case 'Create':
-            return response.send(event, context, response.SUCCESS);
-            break;
-    }
-    response.send(event, context, response.FAILED );
-    // response.send(event, context, response.SUCCESS, {Value: 'test'} )
+
+	console.log(event)
+	if (['Create','Update', 'Delete'].indexOf(event.RequestType) === -1 )
+		return response.send(event, context, response.FAILED );
+
+	var methods = { create: 'PUT', update: 'POST', delete: 'DELETE' }
+
+	var method = methods[ event.RequestType.toLowerCase() ]
+	var resource;
+	switch (event.ResourceType.toLowerCase()) {
+		case 'custom::awspilotgithubtoken':
+			resource = 'grant'
+		default:
+			// unknown resource type
+			return response.send(event, context, response.FAILED );
+	}
+
+	var request_uri = 'https://api.awspilot.com/cf/v1/' + resource
+
+	console.log(methods, request_uri )
+
+	return response.send(event, context, response.SUCCESS);
+	// response.send(event, context, response.SUCCESS, {Value: 'test'} )
 };
