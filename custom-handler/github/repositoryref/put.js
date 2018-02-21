d7343cfbfb14f53361abbba8b09a5433024b8fa1
adrianpraja <adrian@databank.ro>
2018-02-21 12:05:19 +0200

    own cfn-response

--- custom-handler/github/repositoryref/put.js
+++ custom-handler/github/repositoryref/put.js
@@ -1,3 +1,22 @@
+
+get_github_repo = function(repo, token, cb) {
+	request({
+		url     : 'https://api.github.com/repos/' + repo,
+		headers : {authorization : 'token ' + token, 'user-agent'  : 'github-api-basic <https://github.com/awspilot>' },
+		json    : true,
+	}, function (err, r, b) {
+		if (err)
+			return cb(err)
+
+		cb(null, b)
+	});
+}
+
 module.exports = function( event, context ) {
-	cfn.send(event, context, cfn.FAILED, { errorMessage: "Not Implemented" });
+	get_github_repo( event.ResourceProperties.Name , event.ResourceProperties.GithubToken, function(err, data ) {
+		if (err)
+			return cfn.send(event, context, cfn.FAILED, { errorMessage: JSON.stringify(err) });
+
+		return cfn.send(event, context, cfn.SUCCESS, data, data.id );
+	})
 }
