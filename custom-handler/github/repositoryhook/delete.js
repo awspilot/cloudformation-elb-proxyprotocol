9a4f70665aa35702f34cef8e32ee030540a95656
databank <adrian@databank.ro>
2018-02-22 16:02:12 +0200

    github functions in one file

--- custom-handler/github/repositoryhook/delete.js
+++ custom-handler/github/repositoryhook/delete.js
@@ -1,16 +1,3 @@
-delete_github_hook = function(repo, token, hook_id , cb ) {
-	request({
-		url     : 'https://api.github.com/repos/' + repo + '/hooks/' + hook_id,
-		method: 'DELETE',
-		headers : {authorization : 'token ' + token, 'user-agent'  : 'github-api-basic <https://github.com/awspilot>' },
-		json    : true,
-	}, function (err, r, b) {
-		if (err)
-			return cb(err)
-
-		cb(null, b)
-	});
-}
 
 module.exports = function( event, context ) {
 	delete_github_hook( event.ResourceProperties.RepositoryName , event.ResourceProperties.GithubToken,  event.PhysicalResourceId ,function(err, data ) {
